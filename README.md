c0a3adf8ba71fcac4fe2c4a0a53155cf2f76740e
unknown <dzmitry.duboyski@gmail.com>
2023-02-07 04:10:11 +0400

    refactor: update example

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Решение капчи GeeTest с помощью 2captcha-js в Puppeteer
-
-Пример решения капчи GeeTest с помощью плагина 2captcha-js в Puppeteer
+# Решение капчи GeeTest в Puppeteer
 
 ## Алгоритм решения GeeTest:
 1. Найти параметры качи `gt`, `challenge`, `api_server`:
@@ -21,11 +19,11 @@
     >Скриншот:
     ![значение challenge в коде страницы](./screenshot/challenge_value.png)
 
-    !!!FixMe: Подробнее описать про `challenge`
+    Подробнее про challenge можно почитать [тут](https://rucaptcha.com/api-rucaptcha#solving_geetest:~:text=%D0%92%D0%B0%D0%B6%D0%BD%D0%BE%3A%20%D0%B4%D0%BB%D1%8F,%D0%B8%20%D0%BF%D0%B0%D1%80%D1%81%D0%B8%D1%82%D1%8C%20%D0%B5%D0%B3%D0%BE.).
 
 2. Отправка капчи в API.
 
-Для решения капчи, необходимо отправить в API капчу:
+Для решения капчи, необходимо отправить в API найденные параметры капчи:
 ```json
 "key":"your_api_key",
 "method":"geetest"
--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 import puppeteer from 'puppeteer';
 import fetch from 'node-fetch';
-
-// FixMe: use public library
-import Captcha  from '../2captcha-js/dist/index.js';
+import Captcha, { Solver }  from '2captcha-ts';
 import 'dotenv/config';
 const APIKEY = process.env.APIKEY;
+const solver = new Captcha.Solver(APIKEY)
 
-(async () => {
+;(async () => {
   const browser = await puppeteer.launch({
-    devtools: true,
     headless: false
   });
-  const page = await browser.newPage();
+  const [page] = await browser.pages()
 
   await page.setViewport({width: 1080, height: 1024});
   
   // FixMe: what is it "t"
   const t = new Date().getTime()
-
   const response = await fetch(`https://rucaptcha.com/api/v1/captcha-demo/gee-test/init-params?t=${t}`)
 
   const data = await response.json()
@@ -25,66 +22,51 @@ const APIKEY = process.env.APIKEY;
   const gt = data.gt
   const pageurl = 'https://rucaptcha.com/demo/geetest'
 
-  // console.log("challenge: " + challenge)
-  // console.log("data: ")
-  // console.log(data)
-
-  const solver = new Captcha.Solver(APIKEY)
-
+  // Send GeeTest captcha
   const res = await solver.geetest({ gt: gt, challenge: challenge, pageurl: pageurl })
-  
+
   try {
-    // console.log(res.json())
-  const dataCaptcha = res.data
-  const id = res.id
-  console.log(data)
-  console.log(id)
-  const geetest_challenge = dataCaptcha.geetest_challenge
-  const geetest_validate = dataCaptcha.geetest_validate
-  const geetest_seccode = dataCaptcha.geetest_seccode
-
-  console.log('geetest_challenge ' + geetest_challenge)
-  console.log('geetest_validate ' + geetest_validate)
-  console.log('geetest_seccode ' + geetest_seccode)
-  
-  await page.goto('https://rucaptcha.com/demo/geetest');
-
-  await page.waitForSelector('h1')
-  await page.waitForSelector('input[name="geetest_challenge"]')
-  await page.waitForSelector('input[name="geetest_validate"]')
-  await page.waitForSelector('input[name="geetest_seccode"]')
-  await page.waitForSelector('button[type="submit"]')
-
-  const setData = await page.evaluate((data) => {
+    const captchaAnswer = res.data
+    const id = res.id
     console.log(data)
-    console.log(data.geetest_challenge)
-    console.log(data.geetest_validate)
-    console.log(data.geetest_seccode)
-    console.log("dataCaptcha")
+    console.log('captcha_id='+id)
+    const geetest_challenge = captchaAnswer.geetest_challenge
+    const geetest_validate = captchaAnswer.geetest_validate
+    const geetest_seccode = captchaAnswer.geetest_seccode
 
-    document.querySelector('input[name="geetest_challenge"]').value = data.geetest_challenge
-    document.querySelector('input[name="geetest_validate"]').value = data.geetest_validate
-    document.querySelector('input[name="geetest_seccode"]').value = data.geetest_seccode
+    console.log('captcha answer:')
+    console.log(captchaAnswer)
+    
+    await page.goto('https://rucaptcha.com/demo/geetest');
 
-    document.querySelector('button[type="submit"]').click()
-  }, dataCaptcha)
+    await page.waitForSelector('input[name="geetest_challenge"]')
+    await page.waitForSelector('input[name="geetest_validate"]')
+    await page.waitForSelector('input[name="geetest_seccode"]')
+    await page.waitForSelector('button[type="submit"]')
 
-  await page.waitForSelector('#geetest-demo-form div pre code')
+    const setData = await page.evaluate((data) => {
+      document.querySelector('input[name="geetest_challenge"]').value = data.geetest_challenge
+      document.querySelector('input[name="geetest_validate"]').value = data.geetest_validate
+      document.querySelector('input[name="geetest_seccode"]').value = data.geetest_seccode
+      document.querySelector('button[type="submit"]').click()
+    }, captchaAnswer)
 
-  const resultBlockSelector = '#geetest-demo-form div pre code'
-  let result = await page.evaluate(selector => {
-    return document.querySelector(selector).innerText
-  }, resultBlockSelector)
+    await page.waitForSelector('#geetest-demo-form div pre code')
 
-  result = JSON.parse(result)
-  if(result.success) {
-    console.log('Капча успешно решена!')
- }
+    const resultBlockSelector = '#geetest-demo-form div pre code'
+    let result = await page.evaluate(selector => {
+      return document.querySelector(selector).innerText
+    }, resultBlockSelector)
 
- await page.waitForTimeout(5000)
+    result = JSON.parse(result)
+    if(result.success) {
+      console.log('Captcha solved successfully!')
+    } 
 
-} catch(err) {
-  console.error(err.message)
-}
-browser.close()
+    await page.waitForTimeout(5000)
+
+  } catch(err) {
+    console.error(err.message)
+  }
+  browser.close()
 })();
