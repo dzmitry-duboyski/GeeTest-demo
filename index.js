3bb0401ff6417880fe4bd10632fb34c2c3980e76
unknown <dzmitry.duboyski@gmail.com>
2023-02-26 01:18:13 +0400

    refactor: change ruCaptcha to 2captcha, update comments

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ const solver = new Captcha.Solver(APIKEY)
 
   await page.setViewport({width: 1080, height: 1024});
   
-  // FixMe: what is it "t"
   const t = new Date().getTime()
-  const response = await fetch(`https://rucaptcha.com/api/v1/captcha-demo/gee-test/init-params?t=${t}`)
+  // Making a request to the `Geetest` api to get the necessary parameters
+  const response = await fetch(`https://2captcha.com/api/v1/captcha-demo/gee-test/init-params?t=${t}`)
 
   const data = await response.json()
   const challenge = data.challenge
   const gt = data.gt
-  const pageurl = 'https://rucaptcha.com/demo/geetest'
+  const pageurl = 'https://2captcha.com/demo/geetest'
 
-  // Send GeeTest captcha
+  // Send GeeTest captcha to the service to solve
   const res = await solver.geetest({ gt: gt, challenge: challenge, pageurl: pageurl })
 
   try {
+    // Getting a captcha response including a captcha answer
     const captchaAnswer = res.data
     const id = res.id
     console.log(data)
@@ -37,13 +38,14 @@ const solver = new Captcha.Solver(APIKEY)
     console.log('captcha answer:')
     console.log(captchaAnswer)
     
-    await page.goto('https://rucaptcha.com/demo/geetest');
+    await page.goto('https://2captcha.com/demo/geetest');
 
     await page.waitForSelector('input[name="geetest_challenge"]')
     await page.waitForSelector('input[name="geetest_validate"]')
     await page.waitForSelector('input[name="geetest_seccode"]')
     await page.waitForSelector('button[type="submit"]')
 
+    // Using the resulting Geetest captcha solution
     const setData = await page.evaluate((data) => {
       document.querySelector('input[name="geetest_challenge"]').value = data.geetest_challenge
       document.querySelector('input[name="geetest_validate"]').value = data.geetest_validate
